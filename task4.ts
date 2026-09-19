ed15b173be8322af8b39f1d8c4455056b578cbeb
kseniyahirs <kseniya.hirs@gmail.com>
2026-09-20 00:59:55 +0300

    task4

---
+++ task4.ts
@@ -0,0 +1,9 @@
+//ДЗ Lesson3
+//Напишите программу, которая выводит убывающую арифметическую прогрессию начиная от 100 и до 0.
+
+const start: number = 100;
+const end: number = 0;
+
+for (let inp: number = start; inp >= end; inp--) {
+  console.log(inp);
+}
