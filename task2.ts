ae249bb9b6f0b3ecaa3ea4154bb908e5515e49e0
kseniyahirs <kseniya.hirs@gmail.com>
2026-09-20 00:26:49 +0300

    feat task2 and task3

--- task2.ts
+++ task2.ts
@@ -1,9 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const startNum: number = 1;
 const endNum: number = 20;
-
-const result: string = "0";
+let result: string = "0";
 
 //Написать программу, которая выводит сумму всех чисел от 1 до 20
+for (let counter: number = startNum; counter <= endNum; counter++) {
+  //result += counter
+  //result=result+counter
+  result = String(Number(result) + counter);
+}
 
 console.log(result);
